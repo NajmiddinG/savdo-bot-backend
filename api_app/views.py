b5c2b0001b0d353710be842c2692bc57617fbfda
NajmiddinG <najmiddinweb@gmail.com>
2023-06-11 13:47:10 +0500

    page size changed to 10

--- api_app/views.py
+++ api_app/views.py
@@ -9,7 +9,7 @@ class BotUserListCreateApiViewSet(viewsets.ModelViewSet):
 
 # product
 class ProductPagination(pagination.PageNumberPagination):
-    page_size = 1
+    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
 
